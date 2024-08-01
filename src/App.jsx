88ea11680d0fe07c1f13decf4eb8a465a277000c
Ifeoluwah <spencerife.21@gmail.com>
2024-08-01 16:01:11 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.scss';
+import Layout from './components/Layout/Layout';
+
+function App() {
+	return (
+		<>
+			<Layout />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './Layout.module.scss';
+
+const Layout = () => {
+	return (
+		<main>
+			<section>
+				<h1>Mortgage Calculator</h1>
+				<button>Clear All</button>
+			</section>
+		</main>
+	);
+};
+
+export default Layout;
